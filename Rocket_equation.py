bb11925938100874613034759bc0a29d641eaff6
Rowan <16955669+142r1@users.noreply.github.com>
2017-09-09 09:27:34 -0400

    Update Rocket_equation.py
    Changed equation so that it had the proper rocket equation. Added checking if the input was actually a integer.

--- Rocket_equation.py
+++ Rocket_equation.py
@@ -1,13 +1,23 @@
 import math
 
 def rocket_equation(delta_v, exhaust_v, final_m):
-    print(int(-exhaust_v + math.log10(final_m*delta_v)))
+    return(int(final_m * math.pow((2.7),(delta_v)/(exhaust_v))))
+
+def readNumber(Prompt):
+    actualNumber = None7
+    while(actualNumber == None):
+        number = input(Prompt)
+        if number.isdigit():
+            actualNumber = int(number)
+        else:
+            print("That's not a number.")
+    return actualNumber
 
 def main():
     print("Input Delta V, the rocket's exhaust velocity, and the final mass of the rocket, for the initial total mass of the rocket.")
-    delta_v = int(input("Give me delta_v: "))
-    exhaust_v = int(input("Give me exhaust velocity: "))
-    final_m = int(input("Give me final mass of rocket: "))
-    rocket_equation(delta_v, exhaust_v, final_m)
+    delta_v = readNumber("Give me delta_v: ")
+    exhaust_v = readNumber("Give me exhaust velocity: ")
+    final_m = readNumber("Give me final mass of rocket: ")
+    print(rocket_equation(delta_v, exhaust_v, final_m))
 
 main()
