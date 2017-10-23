2bcccd92aaedae4c97dc754732eb9280763665ae
Rowan <rdausilio@gmail.com>
2017-10-23 11:57:02 -0400

    fizz_buzz in Python
    fizz_buzz program in Python for numbers 1-100

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,12 +1,16 @@
-def fizz_buzz():
+def Fizz_buzz():
     for num in range(1,101):
         if num % 3 == 0:
-            print("Fizz")
+            print("fizz")
+            continue
         if num % 5 == 0:
-            print('Buzz')
+            print('buzz')
+            continue
         if num % 5 == 0 and num % 3 == 0:
-            print("FizzBuzz")
+            print("fizzbuzz")
+            continue
         else:
             print(num)
 
-fizz_buzz()
+print("fizz_buzz 1-100")
+Fizz_buzz()
